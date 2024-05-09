1a689ea1062010e841604e78252926dc28ebf855
nicholas-myers <nicholas.myers.professional@gmail.com>
2024-05-09 01:38:06 -0700

    initial project init

---
+++ stories/pdfComponents/formComponents/TextInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const TextInput = ({ }) => (
+    <input type="text" />
+);
+
+TextInput.propTypes = {
+};
+
+TextInput.defaultProps = {
+};
